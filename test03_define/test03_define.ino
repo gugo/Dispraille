17c11e9a589b28eba2b1633b03878335902a3375
gugo <gugo.torelli@gmail.com>
2014-11-14 23:56:55 +0100

    Hey Zach check this out

---
+++ test03_define/test03_define.ino
@@ -0,0 +1,59 @@
+#define SPACE { \
+    {0, 0},  \
+    {0, 0}, \
+    {0, 0}, \
+}
+
+#define H { \
+    {1, 0},  \
+    {1, 1}, \
+    {0, 0}, \
+}
+
+#define E { \
+    {1, 0},  \
+    {0, 1}, \
+    {0, 0}, \
+}
+
+#define L { \
+    {1, 0},  \
+    {1, 0}, \
+    {1, 0}, \
+}
+
+#define O { \
+    {1, 0},  \
+    {0, 1}, \
+    {1, 0}, \
+}
+
+#define W { \
+    {0, 1},  \
+    {1, 1}, \
+    {0, 1}, \
+}
+
+#define R { \
+    {1, 0},  \
+    {1, 1}, \
+    {1, 0}, \
+}
+
+#define D { \
+    {1, 1},  \
+    {1, 0}, \
+    {0, 0}, \
+}
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
